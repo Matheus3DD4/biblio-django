1feb1c05545d68c2bfaae1513810a0212c58cfe2
analauracpaiva <analauracpaiva26@gmail.com>
2023-12-08 09:41:33 -0300

    serializing and viewing

--- biblioteca/serializers/titulo.py
+++ biblioteca/serializers/titulo.py
@@ -1,8 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from biblioteca.models import Titulo
+from uploader.models import Image
 
 
 class TituloSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
     class Meta:
         model = Titulo
         fields = "__all__"
@@ -18,4 +26,5 @@ class TituloDetailSerializer(ModelSerializer):
 class TituloListSerializer(ModelSerializer):
     class Meta:
         model = Titulo
-        fields = ["id", "titulo", "autor"]
+        fields = "__all__"
+        depth = 1
